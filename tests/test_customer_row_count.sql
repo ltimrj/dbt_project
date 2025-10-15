bcd38345d09168d9e9786b41ed0b1271735be413
muthuraj.10826414@ltimindtree.com <muthuraj.10826414@ltimindtree.com>
2025-10-15 11:46:23 +0000

    v 1.7

--- tests/test_customer_row_count.sql
+++ tests/test_customer_row_count.sql
@@ -1,6 +1,8 @@
-
 SELECT
-  CASE WHEN
-    (SELECT COUNT(*) FROM {{ ref('customer') }}) =
-    (SELECT COUNT(*) FROM {{ ref('customer_tgt') }})
-  THEN 0 ELSE 1 END AS test_result
+  src_count,
+  tgt_count,
+  CASE WHEN src_count = tgt_count THEN 0 ELSE 1 END AS test_result
+FROM (
+  SELECT
+    (SELECT COUNT(*) FROM {{ ref('customer') }}) AS src_count,
+    (SELECT COUNT(*) FROM {{ ref('customer_tgt') }}) AS tgt_count
--- tests/test_customer_tgt_duplicates.sql
+++ tests/test_customer_tgt_duplicates.sql
@@ -1,4 +1,4 @@
-
-SELECT customer_id
+SELECT customer_id, COUNT(*) AS duplicate_count
 FROM {{ ref('customer_tgt') }}
 GROUP BY customer_id
+HAVING COUNT(*) > 1
